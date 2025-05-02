bcb244cf5ddfa4ebc6c8e5547fda9fadfec6dbd6
igormanoels <s.igormanoel@gmail.com>
2025-05-01 21:48:36 -0300

    Continuação no desenvolvimento do protótipo e no desenvolvimento da página index do projeto.

--- carometro/src/main/resources/static/js/main.js
+++ carometro/src/main/resources/static/js/main.js
@@ -2,7 +2,23 @@
 const scriptPrincipal = document.getElementById("principal");
 
 function recuperarSenha() {
-    alert('recuperando senha')
+    scriptPrincipal.innerHTML = `
+            <div id="notificacoes">
+                <h1 class="titulo">Encontre sua conta</h1>
+                <p class="texto">Insira seu <strong>email</strong> ou número de <strong>telefone</strong> para recuperar sua conta.</p>
+            </div>
+            
+            <div id="formularioAcesso">
+                <form id="formulario">
+                    <input type="text" id="nome" placeholder="Nome">
+                    <button type="button" id="voltar">voltar</button>
+                    <button type="submit">Recuperar senha</button>
+                </form>
+            </div>
+    `;
+
+    const btnVoltar = document.getElementById('voltar');
+    btnVoltar.addEventListener("click", telaInicial)
 }
 
 function registrar() {
@@ -20,8 +36,8 @@ function registrar() {
                     <input type="text" id="telefone" placeholder="Telefone: XX-XXXXX-XXXX">
                     <input type="password" id="senha" placeholder="Digite sua senha">
                     <input type="password" id="senhaRepetida" placeholder="Repita sua senha">
-                    <button type="submit">Registar-se</button>
                     <button type="button" id="voltar">voltar</button>
+                    <button type="submit">Registar-se</button>
                 </form>
             </div>
     `;
