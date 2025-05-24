a38c23dd2c7d81859ee46ae308c4d2dba40b9f60
igormanoels <s.igormanoel@gmail.com>
2025-05-23 21:11:30 -0300

    Continuação no desenvolvimento da API.

--- carometro/src/main/java/edu/fatec/carometro/model/TipoAcessoUsuarioId.java
+++ carometro/src/main/java/edu/fatec/carometro/model/TipoAcessoUsuarioId.java
@@ -1,6 +1,7 @@
 package edu.fatec.carometro.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.Embeddable;
 
@@ -9,17 +10,33 @@ public class TipoAcessoUsuarioId implements Serializable {
     private Integer tipoAcessoId;
     private String usuarioEmail;
     
-	public Integer getTipoAcessoId() {
-		return tipoAcessoId;
-	}
-	public void setTipoAcessoId(Integer tipoAcessoId) {
-		this.tipoAcessoId = tipoAcessoId;
-	}
-	public String getUsuarioEmail() {
-		return usuarioEmail;
-	}
-	public void setUsuarioEmail(String usuarioEmail) {
-		this.usuarioEmail = usuarioEmail;
-	}
-    
+    public Integer getTipoAcessoId() {
+        return tipoAcessoId;
+    }
+
+    public void setTipoAcessoId(Integer tipoAcessoId) {
+        this.tipoAcessoId = tipoAcessoId;
+    }
+
+    public String getUsuarioEmail() {
+        return usuarioEmail;
+    }
+
+    public void setUsuarioEmail(String usuarioEmail) {
+        this.usuarioEmail = usuarioEmail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TipoAcessoUsuarioId)) return false;
+        TipoAcessoUsuarioId that = (TipoAcessoUsuarioId) o;
+        return Objects.equals(tipoAcessoId, that.tipoAcessoId) &&
+               Objects.equals(usuarioEmail, that.usuarioEmail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tipoAcessoId, usuarioEmail);
+    }
 }
